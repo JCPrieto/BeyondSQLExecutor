7a72984162d3eb247eb86d246ede3f2fc893c518
Juan Carlos Prieto <JuanC.Prieto.Silos@gmail.com>
2024-11-24 21:52:12 +0100

    JDK 17

--- src/main/java/es/jklabs/utilidades/Constantes.java
+++ src/main/java/es/jklabs/utilidades/Constantes.java
@@ -3,7 +3,7 @@ package es.jklabs.utilidades;
 public class Constantes {
     public static final String NOMBRE_APP = "BeyondSQLExecutor";
     public static final String VERSION = "0.4.2";
-    public static final String COMPILACION = "JDK21";
+    public static final String COMPILACION = "JDK17";
 
     private Constantes() {
 
