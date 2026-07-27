f5a97aec451f10c544a143127d553407f06ea875
Juan Carlos Prieto <JuanC.Prieto.Silos@gmail.com>
2026-07-27 17:48:32 +0200

    Test unitarios

--- src/main/java/es/jklabs/security/SwingPasswordPrompt.java
+++ src/main/java/es/jklabs/security/SwingPasswordPrompt.java
@@ -4,14 +4,29 @@ import javax.swing.*;
 import java.awt.*;
 
 class SwingPasswordPrompt implements PasswordPrompt {
+    private final ConfirmDialog confirmDialog;
+
+    SwingPasswordPrompt() {
+        this(JOptionPane::showConfirmDialog);
+    }
+
+    SwingPasswordPrompt(ConfirmDialog confirmDialog) {
+        this.confirmDialog = confirmDialog;
+    }
+
     @Override
     public char[] request(Component parent, String title) {
         JPasswordField field = new JPasswordField(20);
-        int result = JOptionPane.showConfirmDialog(parent, field, title, JOptionPane.OK_CANCEL_OPTION,
+        int result = confirmDialog.show(parent, field, title, JOptionPane.OK_CANCEL_OPTION,
                 JOptionPane.PLAIN_MESSAGE);
         if (result != JOptionPane.OK_OPTION) {
             return null;
         }
         return field.getPassword();
+    }
+
+    @FunctionalInterface
+    interface ConfirmDialog {
+        int show(Component parent, Object message, String title, int optionType, int messageType);
     }
 }
