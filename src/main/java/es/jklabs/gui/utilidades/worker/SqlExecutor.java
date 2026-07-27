a9848c6b449405ac9ee406308cb6ecf35849b70c
Juan Carlos Prieto <JuanC.Prieto.Silos@gmail.com>
2026-07-27 17:39:20 +0200

    {SonarQube} Reorder the modifiers to comply with the Java Language Specification.

--- src/main/java/es/jklabs/gui/utilidades/worker/SqlExecutor.java
+++ src/main/java/es/jklabs/gui/utilidades/worker/SqlExecutor.java
@@ -31,7 +31,7 @@ public class SqlExecutor extends SwingWorker<Void, Void> implements Serializable
     private final ScriptPanel scriptPanel;
     private final List<String> sentenciasPostgres;
     private final int totalPostreSQL;
-    private transient final Supplier<Component[]> serverComponents;
+    private final transient Supplier<Component[]> serverComponents;
     private final DatabaseExecutor databaseExecutor;
     private transient final ConnectionErrorNotifier connectionErrorNotifier;
     private final SqlErrorPrompt sqlErrorPrompt;
