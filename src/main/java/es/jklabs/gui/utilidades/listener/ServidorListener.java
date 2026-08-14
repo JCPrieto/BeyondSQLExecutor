08fceec3cf302f0ede3ebccb12494fa71e5b6e47
Juan Carlos Prieto <JuanC.Prieto.Silos@gmail.com>
2026-08-14 21:23:06 +0200

    Add unit tests for `ServidorListener` and refactor to support injectable popup factory

--- src/main/java/es/jklabs/gui/utilidades/listener/ServidorListener.java
+++ src/main/java/es/jklabs/gui/utilidades/listener/ServidorListener.java
@@ -7,23 +7,31 @@ import es.jklabs.gui.popup.ServerPopUp;
 import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.function.BiFunction;
 
 public class ServidorListener extends MouseAdapter {
 
     private final MainUI mainUI;
     private final ServerItem servidor;
+    private final BiFunction<MainUI, ServerItem, JPopupMenu> serverPopUpFactory;
     private boolean enable;
 
     public ServidorListener(MainUI mainUI, ServerItem servidor) {
+        this(mainUI, servidor, ServerPopUp::new);
+    }
+
+    ServidorListener(MainUI mainUI, ServerItem servidor,
+                     BiFunction<MainUI, ServerItem, JPopupMenu> serverPopUpFactory) {
         this.mainUI = mainUI;
         this.servidor = servidor;
+        this.serverPopUpFactory = serverPopUpFactory;
         this.enable = true;
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
         if (enable && SwingUtilities.isRightMouseButton(e)) {
-            ServerPopUp serverPopUp = new ServerPopUp(mainUI, servidor);
+            JPopupMenu serverPopUp = serverPopUpFactory.apply(mainUI, servidor);
             serverPopUp.show(e.getComponent(), e.getX(), e.getY());
         }
     }
