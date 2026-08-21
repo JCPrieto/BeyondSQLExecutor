5e2dc87c5cbfb321190bf6f6e76c7a4ca66c2338
Juan Carlos Prieto <JuanC.Prieto.Silos@gmail.com>
2026-08-21 19:26:57 +0200

    Refactor: Use try-with-resources for stream handling in `UTF8ResourceBundleControl` to ensure proper closure

--- src/main/java/es/jklabs/utilidades/UTF8ResourceBundleControl.java
+++ src/main/java/es/jklabs/utilidades/UTF8ResourceBundleControl.java
@@ -32,11 +32,9 @@ public class UTF8ResourceBundleControl extends ResourceBundle.Control {
             stream = loader.getResourceAsStream(resourceName);
         }
         if (stream != null) {
-            try {
+            try (InputStreamReader reader = new InputStreamReader(stream, StandardCharsets.UTF_8)) {
                 // Only this line is changed to make it to read properties files as UTF-8.
-                bundle = new PropertyResourceBundle(new InputStreamReader(stream, StandardCharsets.UTF_8));
-            } finally {
-                stream.close();
+                bundle = new PropertyResourceBundle(reader);
             }
         }
         return bundle;
